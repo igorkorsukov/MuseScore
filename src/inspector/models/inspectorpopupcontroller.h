c45a2d7a70e5c5769a88374c2447d2d520f65d48
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2021-12-13 20:57:47 +0100

    Q_PROPERTY types must be complete types

--- src/inspector/models/inspectorpopupcontroller.h
+++ src/inspector/models/inspectorpopupcontroller.h
@@ -28,11 +28,14 @@
 #include "modularity/ioc.h"
 #include "ui/imainwindow.h"
 
+Q_MOC_INCLUDE(< QQuickItem >)
+Q_MOC_INCLUDE("uicomponents/view/popupview.h")
+
+class QQuickItem;
+
 namespace mu::uicomponents {
 class PopupView;
 }
-
-class QQuickItem;
 
 namespace mu::inspector {
 class InspectorPopupController : public QObject
