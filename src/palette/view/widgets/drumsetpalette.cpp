175c02de5a0924448a7e14879a83772c68454a09
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2021-12-14 00:17:24 +0100

    Replace `QEvent` with `QEnterEvent` in `QWidget::enterEvent` signature

--- src/palette/view/widgets/drumsetpalette.cpp
+++ src/palette/view/widgets/drumsetpalette.cpp
@@ -248,7 +248,7 @@ void DrumsetPalette::mouseMoveEvent(QMouseEvent* event)
     m_drumPalette->handleEvent(event);
 }
 
-void DrumsetPalette::enterEvent(QEvent* event)
+void DrumsetPalette::enterEvent(QEnterEvent* event)
 {
     m_drumPalette->handleEvent(event);
 }
